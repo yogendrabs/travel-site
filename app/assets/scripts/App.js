d099b4edcdeef87387d0c9e6b357c5d5f6811590
YBS <id_ybs@hotmail.com>
2019-12-22 20:35:12 +1100

    Completed menu icon style, while on master branch

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,19 +1,3 @@
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-    payTaxes() {
-
-        console.log("Adult Class");
-
-        console.log(this);
-        
-        console.log(this.name + " pays $0 taxes.");
-    }
-}
-
-var john = new Person("John", "blue");
-john.greet();
-
-var jane = new Adult("Jane", "pink");
-jane.greet();
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
