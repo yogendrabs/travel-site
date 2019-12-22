5207e427ae9395421a759b821f97d6d580a00436
YBS <id_ybs@hotmail.com>
2019-12-22 22:04:09 +1100

    completed reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScrol from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScrol($(".feature-item"), "85%");
+new RevealOnScrol($(".testimonial"), "60%");
+
