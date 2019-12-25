43726394014112b855d4c8e43c7bb9a425bb07f4
YBS <id_ybs@hotmail.com>
2019-12-25 13:05:38 +1100

    Complete lazyLoading

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10435,6 +10435,7 @@ function () {
   function StickyHeader() {
     _classCallCheck(this, StickyHeader);
 
+    this.lazyImages = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".lazyload");
     this.siteHeader = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".site-header");
     this.headerTriggerElement = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".large-hero__title");
     this.createHeaderWaypoint();
@@ -10442,9 +10443,17 @@ function () {
     this.headerLinks = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".primary-nav a");
     this.createPageSectionWaypoints();
     this.addSmoothScrolling();
+    this.refreshWaypoints();
   }
 
   _createClass(StickyHeader, [{
+    key: "refreshWaypoints",
+    value: function refreshWaypoints() {
+      this.lazyImages.on('load', function () {
+        Waypoint.refreshAll();
+      });
+    }
+  }, {
     key: "addSmoothScrolling",
     value: function addSmoothScrolling() {
       this.headerLinks.smoothScroll();
